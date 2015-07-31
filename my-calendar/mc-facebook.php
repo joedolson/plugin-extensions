9e9abceab20d1795f0052d7989175618438a302b
joedolson <design@joedolson.com>
2015-07-31 12:12:04 -0500

    Add documentation to mc-facebook
    Ultimately, want all of these to have docs.

--- my-calendar/mc-facebook.php
+++ my-calendar/mc-facebook.php
@@ -8,6 +8,16 @@ Version: 1.0.0
 Author URI: http://www.joedolson.com/
 */
 
+/**
+ * This plug-in adds a template tag to the library of My Calendar template tags to add a 'share' link for the event. 
+ *
+ * Usage: {facebook}
+ *
+ * @param array $e Array of template tags.
+ * @param object $event Event Object.
+ *
+ * @return array $e
+ */
 add_filter( 'mc_filter_shortcodes', 'mc_facebook', 10, 2 );
 function mc_facebook( $e, $event ) {
 	$e['facebook'] = "<a href='https://www.facebook.com/sharer.php?u='" . urlencode( mc_get_details_link( $event ) ) . "'>Share on Facebook</a>";
