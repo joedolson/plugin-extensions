b0f03b7e26780708b2259958cf74875479f4892d
joedolson <design@joedolson.com>
2015-08-08 09:52:27 -0500

    Add since reference.
    For usage only since WP Accessibility 1.4.3

--- wp-accessibility/custom-fontsize-css.php
+++ wp-accessibility/custom-fontsize-css.php
@@ -12,6 +12,8 @@
  * Filter the CSS file for the WP Accessibility font size styles.
  * This example returns the URL for a file named 'fontsize.css' and placed in your theme or child theme directory.
  * 
+ * @since WP Accessibility 1.4.3
+ * 
  * @param string URL to default stylesheet
  * 
  * @return mixed boolean/string Return false to disable the stylesheet; return a URL resolving to your custom styles to replace it.
