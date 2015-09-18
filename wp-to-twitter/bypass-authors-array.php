4878d042d817c8509226698692b9b88e7ccb452d
joedolson <design@joedolson.com>
2015-09-18 18:09:58 -0500

    Better comments in bypass authors
    'main'=>false is confusing, so I added a comment to explain it.

--- wp-to-twitter/bypass-authors-array.php
+++ wp-to-twitter/bypass-authors-array.php
@@ -13,10 +13,12 @@ add_filter( 'wpt_filter_users', 'include_main_account', 10, 2 );
 /*
 *	Passes post author and main account into authors array regardless of settings. 
 * 	$authors = array of user IDs to Tweet to
+*   Despite appearances, using 'main'=>false adds the Main account into the array. 
 */
 function include_main_account( $authors, $post_info ) {
 	if ( wtt_oauth_test( $post_info['authId'], 'verify' ) ) {
-		$authors = array( $post_info['authId'], 'main'=>false );
+		// $authors = array( 14, 'main'=>false ); 	// if you want a specific Author ID
+		$authors = array( $post_info['authId'], 'main'=>false ); // if you want the author of the post being published
 	} else {
 		$authors = array( 'main'=>false );
 	}
