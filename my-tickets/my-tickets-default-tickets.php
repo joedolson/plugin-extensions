2ff308499460707af243e7109bb0b02972e08a89
joedolson <design@joedolson.com>
2015-09-23 12:11:49 -0500

    Add default ticket modifier
    Automatically set for 1 ticket

---
+++ my-tickets/my-tickets-default-tickets.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Add one ticket by default
+Plugin URI: http://www.joedolson.com/my-tickets/
+Description: By default, My Tickets requires you to check a box or select a number of tickets before adding to cart. This makes the default to be checked/one ticket.
+Version: 1.0.0
+Author: Joseph Dolson
+Author URI: http://www.joedolson.com/
+*/
+
+/**
+ * Set the default number of tickets to add to cart to 1
+ */
+add_filter( 'mt_cart_default_value', 'my_cart_default_value', 10, 1 );
+function my_cart_default_value( $default ) {
+	return 1; // automatically add to cart on button press, instead of requiring user action.
+}
