3d114a61b2379fdb80fc98ac75a9153fd96fa2b0
joedolson <design@joedolson.com>
2016-02-12 11:11:34 -0600

    Add filter for ticket type text

--- my-tickets/my-tickets-filter-text.php
+++ my-tickets/my-tickets-filter-text.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Customize Text in My Tickets
+Plugin Name: Customize Ticket Type Notification Text
 Plugin URI: http://www.joedolson.com/
 Description: Example plug-in demonstrating how to replace text using filters in My Tickets.
 Version: 1.0.0
@@ -9,12 +9,21 @@ Author URI: http://www.joedolson.com/
 */
 
 /**
- * Replace add to cart text using the mt_add_to_cart_text filter
+ * Replace add to cart text using the mt_render_ticket_type_message filter
  *
- * This is an example using the 'mt_add_to_cart_text' filter, which replaces the text for the Add to Cart button.
- * There are many text filters available in My Tickets beyond this example.
+ * @param string $text The default text, wrapped in a paragraph with class 'ticket-type-message'
+ * @param string $type The type of ticket being bought; either 'eticket', 'printable', 'postal', or 'willcall'
+ * This is an example using the 'mt_render_ticket_type_message' filter, 
+ * which replaces the text for all ticket type notifications
  */
-add_filter( 'mt_add_to_cart_text', 'my_add_to_cart_text', 10, 1 );
-function my_add_to_cart_text( $default ) {
-	return "Add Tickets to Cart"; // your custom text
+add_filter( 'mt_render_ticket_type_message', 'my_ticket_type_text', 10, 2 );
+function my_ticket_type_text( $text, $type ) {
+	if ( $type == 'willcall' ) {
+		$text = "Your ticket will be available under your name at the box office 30 minutes before the show."; 
+	}
+	if ( $type == 'eticket' ) {
+		$text = "You will receive a link to view your ticket once payment is completed.";
+	}
+	
+	return $text;
 }
